b9a8a2660282e118b37f5bdd5c02003cd79d5e13
Agus S. Mubarok <gusrylmubarok@gmail.com>
2022-09-29 20:56:54 +0700

    Make Models and Migrations

--- bookr/reviews/views.py
+++ bookr/reviews/views.py
@@ -4,3 +4,8 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, "base.html")
+
+
+def book_search(request):
+    search_text = request.GET.get("search", "")
+    return render(request, "search-results.html", {"search_text": search_text})
