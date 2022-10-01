7017abf0a58a3cf8420a21437969b8f02570ce26
Agus S. Mubarok <gusrylmubarok@gmail.com>
2022-10-01 15:43:14 +0700

    Safely Mode

--- core-app/settings.py
+++ core-app/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool("DJANGO_DEBUG")
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
