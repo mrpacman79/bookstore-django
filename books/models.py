92c1f90f58be117afe493e67a7250e956e3b0a34
Agus S. Mubarok <gusrylmubarok@gmail.com>
2022-10-02 12:28:43 +0700

    Application Permissions

--- books/models.py
+++ books/models.py
@@ -4,7 +4,6 @@ from django.db import models
 from django.urls import reverse
 
 
-
 # Books Models
 class Book(models.Model):
     id = models.UUIDField(
@@ -15,6 +14,11 @@ class Book(models.Model):
     author = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     cover = models.ImageField(upload_to="covers/", blank=True)
+
+    class Meta:
+        permissions = [
+            ("special_status", "can read all books"),
+        ]
 
     def __str__(self):
         return self.title
